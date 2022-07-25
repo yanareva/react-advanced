b1bd0f6f83558f3ba2ab59062527aa352e01e233
@reva.y <yana.reva@nixsolutions.com>
2022-07-25 16:17:00 +0300

    add project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Routes, Route} from "react-router-dom";
+import {HomePage} from "./pages/HomePage";
+import {FavoritesPages} from "./pages/FavoritesPages";
+import {Navigation} from "./components/Navigation";
+
+function App() {
+    return (
+        <>
+            <Navigation/>
+            <Routes>
+                <Route path='/' element={<HomePage/>}/>
+                <Route path='/favourites' element={<FavoritesPages/>}/>
+            </Routes>
+        </>
+    );
+}
+
+export default App;
